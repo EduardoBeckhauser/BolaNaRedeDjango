bb316fa315b9a10f3b8e3d2bbec6bf49e0613e86
EduardoBeckhauser <eduardololamorim235@gmail.com>
2023-11-17 09:15:59 -0300

    2

--- bolanarede/views.py
+++ bolanarede/views.py
@@ -1,8 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
+from django_filters.rest_framework import DjangoFilterBackend
+
+
 from bolanarede.models import Camisa
 from bolanarede.serializers import CamisaSerializer
+
 
 class CamisaViewSet(ModelViewSet):
     queryset = Camisa.objects.all()
     serializer_class = CamisaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["time__nome"]

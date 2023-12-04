d4ab6d80c4aa51dc0a3e518c29b0ca5b65779003
EduardoBeckhauser <eduardololamorim235@gmail.com>
2023-12-04 09:17:51 -0300

    quase terminando

--- bolanarede/views.py
+++ bolanarede/views.py
@@ -2,13 +2,18 @@ from rest_framework.viewsets import ModelViewSet
 
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from bolanarede.models import Camisa
 from bolanarede.serializers import CamisaSerializer
 
 
+
 class CamisaViewSet(ModelViewSet):
     queryset = Camisa.objects.all()
     serializer_class = CamisaSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["time__nome"]
+    search_fields = ["nome"]
+    ordering_fields = ["nome", "preco"]
+    ordering = ["nome"]
